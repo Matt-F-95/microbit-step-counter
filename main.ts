1a720860fded33ae564f4e4e668d37f668974662
Matt Felton <mattfelton95@gmail.com>
2021-03-23 19:13:12 -0400

    changed messages to show strings on step goal reached

--- main.ts
+++ main.ts
@@ -1,93 +1,40 @@
+input.onButtonPressed(Button.AB, function () {
+    steps = 0
+    basic.showNumber(steps)
+})
 input.onGesture(Gesture.Shake, function () {
     steps += 1
     basic.showNumber(steps)
 })
 let steps = 0
 steps = 0
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . # # # .
-    . # . # .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . # . .
-    . . # . .
-    . . # . .
-    . . . . .
-    `)
+basic.showIcon(IconNames.StickFigure)
+basic.showString("PRESS A + B TO RESET")
+basic.showArrow(ArrowNames.East)
+basic.showArrow(ArrowNames.West)
 basic.forever(function () {
     if (steps == 25) {
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # . # #
-            . # . . #
-            . # # # #
-            `)
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . . # . .
-            . # . . .
-            . # # # .
-            `)
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+        basic.showString("GREAT START!")
     }
     if (steps == 50) {
         music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # . # #
-            . # . . #
-            . # # # #
-            `)
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . . # . .
-            . # . . .
-            . # # # .
-            `)
+        basic.showString("KEEP IT UP!")
     }
     if (steps == 100) {
-        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
-        basic.showLeds(`
-            . . . . #
-            . . . # .
-            # . # . .
-            . # . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # . # #
-            . # . . #
-            . # # # #
-            `)
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . . # . .
-            . # . . .
-            . # # # .
-            `)
+        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+        basic.showString("AMAZING!")
+    }
+    if (steps == 250) {
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+        basic.showString("NICE!")
+    }
+    if (steps == 500) {
+        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+        basic.showString("OUTSTANDING!")
+    }
+    if (steps == 1000) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        basic.showString("OLYMPIAN!")
     }
 })
