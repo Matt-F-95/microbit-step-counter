ca246f11352654c731ecb7142e64fd5f781374e1
Matt Felton <mattfelton95@gmail.com>
2021-03-23 19:55:40 -0400

    changed step counting achievement times and icons

--- main.ts
+++ main.ts
@@ -6,35 +6,68 @@ input.onGesture(Gesture.Shake, function () {
     steps += 1
     basic.showNumber(steps)
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    steps += 100
+    basic.showNumber(steps)
+})
 let steps = 0
+music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
 steps = 0
-basic.showIcon(IconNames.StickFigure)
 basic.showString("PRESS A + B TO RESET")
 basic.showArrow(ArrowNames.East)
 basic.showArrow(ArrowNames.West)
+basic.showNumber(steps)
 basic.forever(function () {
-    if (steps == 25) {
-        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
-        basic.showString("GREAT START!")
-    }
-    if (steps == 50) {
-        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
-        basic.showString("KEEP IT UP!")
-    }
     if (steps == 100) {
-        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
-        basic.showString("AMAZING!")
-    }
-    if (steps == 250) {
-        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
-        basic.showString("NICE!")
+        music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+        basic.showIcon(IconNames.Yes)
     }
     if (steps == 500) {
-        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
-        basic.showString("OUTSTANDING!")
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        basic.showIcon(IconNames.Happy)
     }
     if (steps == 1000) {
+        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+    if (steps == 2000) {
+        music.startMelody(music.builtInMelody(Melodies.Ringtone), MelodyOptions.Once)
+        basic.showIcon(IconNames.Heart)
+    }
+    if (steps == 5000) {
+        music.startMelody(music.builtInMelody(Melodies.Blues), MelodyOptions.Once)
+        basic.showIcon(IconNames.Butterfly)
+    }
+    if (steps == 7500) {
         music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
-        basic.showString("OLYMPIAN!")
+        basic.showIcon(IconNames.StickFigure)
+    }
+    if (steps == 10000) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        basic.showIcon(IconNames.Sword)
+    }
+    if (steps == 12500) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        basic.showIcon(IconNames.Pitchfork)
+    }
+    if (steps == 15000) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        basic.showIcon(IconNames.LeftTriangle)
+    }
+    if (steps == 17500) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        basic.showIcon(IconNames.Asleep)
+    }
+    if (steps == 20000) {
+        music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
+        basic.showIcon(IconNames.Silly)
+    }
+    if (steps == 25000) {
+        music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+        basic.showIcon(IconNames.Skull)
+    }
+    if (steps == 30000) {
+        music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.ForeverInBackground)
+        basic.showIcon(IconNames.Ghost)
     }
 })
