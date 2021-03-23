bafd46bf7b6681b7a89e83be77147daf3f04f089
Matt Felton <mattfelton95@gmail.com>
2021-03-23 18:53:47 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,93 @@
+input.onGesture(Gesture.Shake, function () {
+    steps += 1
+    basic.showNumber(steps)
+})
+let steps = 0
+steps = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . # # # .
+    . # . # .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . . # . .
+    . . # . .
+    . . . . .
+    `)
 basic.forever(function () {
-	
+    if (steps == 25) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # . # #
+            . # . . #
+            . # # # #
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . # . .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (steps == 50) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # . # #
+            . # . . #
+            . # # # #
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . # . .
+            . # . . .
+            . # # # .
+            `)
+    }
+    if (steps == 100) {
+        music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # # # .
+            . # . . .
+            . # . # #
+            . # . . #
+            . # # # #
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . . # .
+            . . # . .
+            . # . . .
+            . # # # .
+            `)
+    }
 })
